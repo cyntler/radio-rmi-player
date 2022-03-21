b29613f6c4005606974ba5914c44cbf3c4334607
Damian Cyntler <damian@cyntler.com>
2022-03-21 01:29:38 +0100

    Refactor project setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
+import App from './App'
+import './index.css'
+
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 )
