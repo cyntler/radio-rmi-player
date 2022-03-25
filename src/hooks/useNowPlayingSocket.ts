3d255e9e5deaef5ce8ffc78debcf97a97c9e7713
Damian Cyntler <damian@cyntler.com>
2022-03-25 21:03:55 +0100

    Add initial loading

--- src/hooks/useNowPlayingSocket.ts
+++ src/hooks/useNowPlayingSocket.ts
@@ -1,27 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
 import { Song } from '../models';
 
+const SERVER_URL = 'server.radiormi.pl';
+const STATION_NAME = 'radiormi';
+
 export const useNowPlayingSocket = () => {
   const { lastJsonMessage } = useWebSocket(
-    'wss://server.radiormi.pl/api/live/nowplaying/radiormi'
+    `wss://${SERVER_URL}/api/live/nowplaying/${STATION_NAME}`
   );
   const [listenUrl, setListenUrl] = useState('');
   const [song, setSong] = useState<Song | undefined>();
+  const [description, setDescription] = useState('');
+
+  const setData = (jsonMessage: any) => {
+    setListenUrl(jsonMessage.station?.listen_url);
+    setDescription(jsonMessage.station?.description);
+    setSong({
+      artist: jsonMessage.now_playing?.song?.artist,
+      title: jsonMessage.now_playing?.song?.title,
+      coverUrl: jsonMessage.now_playing?.song?.art,
+    });
+  };
+
+  const fetchInitialData = useCallback(async () => {
+    const response = await fetch(
+      `https://${SERVER_URL}/api/nowplaying_static/${STATION_NAME}.json`
+    );
+    const jsonData = await response.json();
+
+    if (jsonData) {
+      setData(jsonData);
+    }
+  }, []);
 
   useEffect(() => {
-    if (!lastJsonMessage) return;
-
-    setListenUrl(lastJsonMessage.station?.listen_url);
-    setSong({
-      artist: lastJsonMessage.now_playing?.song?.artist,
-      title: lastJsonMessage.now_playing?.song?.title,
-      coverUrl: lastJsonMessage.now_playing?.song?.art,
-    });
+    if (lastJsonMessage) {
+      setData(lastJsonMessage);
+    }
   }, [lastJsonMessage]);
+
+  useEffect(() => {
+    fetchInitialData();
+  }, [fetchInitialData]);
 
   return {
     listenUrl,
     song,
+    description,
   };
 };
