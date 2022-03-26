1f37c19f22d75a73ee540892297b051b5d3c2749
Damian Cyntler <damian@cyntler.com>
2022-03-26 18:39:58 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig({
-  plugins: [react()],
+import { version } from './package.json';
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        entry: './src/index.tsx',
+        inject: {
+          data: {
+            version,
+            title: process.env.VITE_APP_NAME,
+          },
+        },
+      }),
+    ],
+  };
 });
