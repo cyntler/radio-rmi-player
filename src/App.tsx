911a943d756ab5452f2009d5343827f3197e2988
Damian Cyntler <damian@cyntler.com>
2022-07-23 22:48:14 +0200

    Implement station context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Background } from './components/Background';
 import { Logo } from './components/Logo';
 import { Player } from './components/Player';
+import { StationContextProvider } from './contexts/stationContext';
 
 export const App = () => (
-  <>
+  <StationContextProvider>
     <Background />
     <Logo />
     <Player />
-  </>
+  </StationContextProvider>
 );
