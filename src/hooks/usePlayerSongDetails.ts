fcfea65c4843ede42939da244da874c6513da735
Damian Cyntler <damian@cyntler.com>
2022-07-24 15:43:50 +0200

    Add player song details hook

---
+++ src/hooks/usePlayerSongDetails.ts
@@ -0,0 +1,69 @@
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
+
+import { stationContext } from '../contexts/stationContext';
+import {
+  CURRENT_SONG_HEADING,
+  NEXT_SONG_HEADING,
+  SHOW_NEXT_SONG_AFTER_SECONDS,
+  SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS,
+} from '../models';
+import { getFullSongName } from '../utils/getFullSongName';
+
+export const usePlayerSongDetails = () => {
+  const { isPlaying, song, nextSong } = useContext(stationContext);
+  const nextSongTimeout = useRef<NodeJS.Timer>();
+  const [heading, setHeading] = useState(CURRENT_SONG_HEADING);
+  const [title, setTitle] = useState('');
+
+  const clearNextSongTimeout = () => {
+    if (nextSongTimeout.current) {
+      clearTimeout(nextSongTimeout.current);
+    }
+  };
+
+  const showCurrentSong = useCallback(() => {
+    if (song) {
+      setHeading(CURRENT_SONG_HEADING);
+      setTitle(getFullSongName(song.artist, song.title));
+    }
+  }, [song]);
+
+  const showNextSongTimeoutStart = useCallback(() => {
+    clearNextSongTimeout();
+
+    if (nextSong) {
+      nextSongTimeout.current = setTimeout(() => {
+        setHeading(NEXT_SONG_HEADING);
+        setTitle(getFullSongName(nextSong.artist, nextSong.title));
+        setTimeout(() => {
+          showCurrentSong();
+          showNextSongTimeoutStart();
+        }, 1000 * SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS);
+      }, 1000 * SHOW_NEXT_SONG_AFTER_SECONDS);
+    }
+  }, [showCurrentSong, nextSong]);
+
+  useEffect(() => {
+    clearNextSongTimeout();
+
+    if (song) {
+      setHeading(CURRENT_SONG_HEADING);
+      setTitle(getFullSongName(song.artist, song.title));
+    }
+  }, [song]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      showNextSongTimeoutStart();
+      return;
+    }
+
+    clearNextSongTimeout();
+  }, [isPlaying, nextSong, showNextSongTimeoutStart]);
+
+  return {
+    heading,
+    title,
+    isNextSongShown: heading === NEXT_SONG_HEADING,
+  };
+};
