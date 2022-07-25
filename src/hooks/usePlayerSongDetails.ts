6ddd30d7a4827b754a0b749f6a06e7068b5a9868
Damian Cyntler <damian@cyntler.com>
2022-07-25 17:14:15 +0200

    Limit next song changing

--- src/hooks/usePlayerSongDetails.ts
+++ src/hooks/usePlayerSongDetails.ts
@@ -5,6 +5,7 @@ import {
   CURRENT_SONG_HEADING,
   NEXT_SONG_HEADING,
   SHOW_NEXT_SONG_AFTER_SECONDS,
+  SHOW_NEXT_SONG_AGAIN_AFTER_SECONDS,
   SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS,
 } from '../models';
 import { getFullSongName } from '../utils/getFullSongName';
@@ -28,20 +29,25 @@ export const usePlayerSongDetails = () => {
     }
   }, [song]);
 
-  const showNextSongTimeoutStart = useCallback(() => {
-    clearNextSongTimeout();
+  const showNextSongTimeoutStart = useCallback(
+    (timeout?: number) => {
+      clearNextSongTimeout();
 
-    if (nextSong) {
-      nextSongTimeout.current = setTimeout(() => {
-        setHeading(NEXT_SONG_HEADING);
-        setTitle(getFullSongName(nextSong.artist, nextSong.title));
-        setTimeout(() => {
-          showCurrentSong();
-          showNextSongTimeoutStart();
-        }, 1000 * SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS);
-      }, 1000 * SHOW_NEXT_SONG_AFTER_SECONDS);
-    }
-  }, [showCurrentSong, nextSong]);
+      if (nextSong) {
+        nextSongTimeout.current = setTimeout(() => {
+          setHeading(NEXT_SONG_HEADING);
+          setTitle(getFullSongName(nextSong.artist, nextSong.title));
+          setTimeout(() => {
+            showCurrentSong();
+            if (!timeout) {
+              showNextSongTimeoutStart(SHOW_NEXT_SONG_AGAIN_AFTER_SECONDS);
+            }
+          }, 1000 * SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS);
+        }, 1000 * (timeout || SHOW_NEXT_SONG_AFTER_SECONDS));
+      }
+    },
+    [showCurrentSong, nextSong]
+  );
 
   useEffect(() => {
     clearNextSongTimeout();
@@ -59,7 +65,7 @@ export const usePlayerSongDetails = () => {
     }
 
     clearNextSongTimeout();
-  }, [isPlaying, nextSong, showNextSongTimeoutStart]);
+  }, [isPlaying, showNextSongTimeoutStart]);
 
   return {
     heading,
--- src/models.ts
+++ src/models.ts
@@ -27,7 +27,8 @@ export const SERVER_STATION_NAME = 'radiormi';
 export const WEBSOCKET_API_URL = `wss://${SERVER_URL}/api/live/nowplaying/${SERVER_STATION_NAME}`;
 export const JSON_API_URL = `https://${SERVER_URL}/api/nowplaying_static/${SERVER_STATION_NAME}.json`;
 
-export const CURRENT_SONG_HEADING = 'Aktualnie gramy';
-export const NEXT_SONG_HEADING = 'Następnie';
+export const CURRENT_SONG_HEADING = 'Teraz gramy';
+export const NEXT_SONG_HEADING = 'Następnie zagramy';
 export const SHOW_NEXT_SONG_AFTER_SECONDS = 30;
-export const SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS = 5;
+export const SHOW_NEXT_SONG_AGAIN_AFTER_SECONDS = 90;
+export const SHOW_PREVIOUSLY_CURRENT_SONG_AFTER_SECONDS = 10;
