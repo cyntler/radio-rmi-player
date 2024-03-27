e4398bae449647ea4ce942e6796efbddbf2d6e13
Damian Cyntler <damian@cyntler.com>
2024-03-27 14:22:27 +0100

    feat: refactor logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,29 +1,38 @@
 import { FunctionComponent, useContext } from 'react';
 
+import { version } from '../../../package.json';
 import { stationContext } from '../../contexts/stationContext';
-import { assets } from '../../models';
+import { PlayerStatus, assets } from '../../models';
 import {
   LogoContainer,
   LogoImage,
   LogoLink,
   SupportButton,
+  SupportInfoContainer,
 } from './Logo.style';
 
 export const Logo: FunctionComponent = () => {
-  const { isPlaying } = useContext(stationContext);
+  const { status } = useContext(stationContext);
 
   return (
     <LogoContainer>
       <LogoLink href="/">
-        <LogoImage src={assets.logo} alt="Radio RMI" isRotation={isPlaying} />
+        <LogoImage
+          src={assets.logo}
+          alt="Radio RMI"
+          $isRotation={status === PlayerStatus.PLAYING}
+        />
       </LogoLink>
-      <SupportButton
-        href="https://zrzutka.radiormi.pl/wplac"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Wspomóż nas
-      </SupportButton>
+      <SupportInfoContainer>
+        <SupportButton
+          href="https://zrzutka.radiormi.pl/wplac"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Wspomóż nas
+        </SupportButton>
+        <small>v{version}</small>
+      </SupportInfoContainer>
     </LogoContainer>
   );
 };
